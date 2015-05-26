154676773289019219fc30ee48d92b8fa65db246
QihongL <lvqihong1992@gmail.com>
2015-05-26 14:14:28 -0500

    fixed syntax errors in main.m

--- ProjectTemplate/main.m
+++ ProjectTemplate/main.m
@@ -1,26 +1,26 @@
 function results = main()
-	%% Set Variables
+  %% Set Variables
   PARAMS = loadjson('PARAMS.json');
   if isfield(PARAMS, 'MaxIter')
-    MaxIter = PARAMS['MaxIter'];
+    MaxIter = PARAMS.MaxIter;
   else
     MaxIter = 0;
   end
-	opts = glmnetSet();
-	opts.alpha = 1; % 1 means LASSO; 0 means Ridge
-	[Y, CVBLOCKS] = loadMetadata(PARAMS['metadata'], PARAMS['TargetCategory']);
+  opts = glmnetSet();
+  opts.alpha = 1; % 1 means LASSO; 0 means Ridge
+  [Y, CVBLOCKS] = loadMetadata(PARAMS.metadata, PARAMS.TargetCategory);
 
   %% Start the loop
   for ss = 1:length(PARAMS.data)
-		X = loadFuncData(PARAMS.data{ss});
-		CVBLOCKS = CVBLOCKS{ss};
-		Y = Y{ss};
+    X = loadFuncData(PARAMS.data{ss});
+    CVBLOCKS = CVBLOCKS{ss};
+    Y = Y{ss};
     if MaxIter > 0
-		  results = iterativelasso(X,Y,CVBLOCKS,'MaxIter',MaxIter);
+      results = iterativelasso(X,Y,CVBLOCKS,'MaxIter',MaxIter);
     else
-		  results = iterativelasso(X,Y,CVBLOCKS);
+      results = iterativelasso(X,Y,CVBLOCKS);
     end
-	end
+  end
 end
 
 %% Private Functions
